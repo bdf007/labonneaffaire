d43b4e20e74de491ffdbe15dc38152b31d2880cf
bdf007 <bdf007@gmail.com>
2023-11-20 10:04:15 +0100

    add article component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ArticleComponent } from "./article/article.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, ArticleComponent]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'labonneaffaire';
+  constructor() { }
+  ngOnInit(): void {
+    console.log('Ici ngOninit');
+  }
 }
