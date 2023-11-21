0d632f2d806151a4470c93fbc019d129b41ada81
bdf007 <bdf007@gmail.com>
2023-11-21 11:06:05 +0100

    route ajouter form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,6 @@ export const routes: Routes = [
     {path:"list", redirectTo:""},
     { path: "detail/:id", component: DetailComponent },
     { path: "ajouter", component: AjouterComponent },
-    {path: "ajouterreactif", component: AjouterComponent},
+    { path: "ajouterreactif", component: AjouterComponent },
     {path:"**",component:PageNotFoundComponent},
 ];
